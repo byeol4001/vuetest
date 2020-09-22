ee0e0495595ecd1b003fe196260de0d1fc8e012b
byeol4001 <byeol4001@gmail.com>
2020-09-22 18:01:53 +0900

    마지막 챕터 전까지 완료 vue-cli 설치도 완료

---
+++ vue/vue/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <app-header v-bind:propsdata="text" v-on:renew="renewStr" />
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+export default {
+  data: function() {
+    return {
+      text: 'header',
+    };
+  },
+  components: {
+    AppHeader,
+  },
+  methods: {
+    renewStr: function() {
+      this.text = 'hi';
+    },
+  },
+};
+</script>
+
+<style></style>
